f0a7d5095e31cbb3338fd74996ecce1963677165
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-29 17:53:19 +0200

    Setting up gradle plugin class

---
+++ apm-agent-plugin/src/main/java/co/elastic/apm/android/plugin/ApmAndroidAgentPlugin.java
@@ -0,0 +1,12 @@
+package co.elastic.apm.android.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class ApmAndroidAgentPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}

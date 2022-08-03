f90659a27b1a15fcc5942283a08cc5e82e95f8d1
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-03 13:40:45 +0200

    Applying elastic apm sdk to host project's dependencies

--- android-plugin/src/main/java/co/elastic/apm/android/plugin/ApmAndroidAgentPlugin.java
+++ android-plugin/src/main/java/co/elastic/apm/android/plugin/ApmAndroidAgentPlugin.java
@@ -3,10 +3,16 @@ package co.elastic.apm.android.plugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
+import co.elastic.apm.generated.BuildConfig;
+
 class ApmAndroidAgentPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
+        addSdkDependency(project);
+    }
 
+    private void addSdkDependency(Project project) {
+        project.getDependencies().add("implementation", BuildConfig.SDK_DEPENDENCY_URI);
     }
 }

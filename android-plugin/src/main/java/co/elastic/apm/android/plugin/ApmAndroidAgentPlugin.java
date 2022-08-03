3bdf781a7d1e1b12f7899f1e0728834961c99e79
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-03 13:52:10 +0200

    Applying ByteBuddy android plugin to the host project

--- android-plugin/src/main/java/co/elastic/apm/android/plugin/ApmAndroidAgentPlugin.java
+++ android-plugin/src/main/java/co/elastic/apm/android/plugin/ApmAndroidAgentPlugin.java
@@ -1,5 +1,7 @@
 package co.elastic.apm.android.plugin;
 
+import net.bytebuddy.build.gradle.android.ByteBuddyAndroidPlugin;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -10,6 +12,11 @@ class ApmAndroidAgentPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         addSdkDependency(project);
+        addBytebuddyPlugin(project);
+    }
+
+    private void addBytebuddyPlugin(Project project) {
+        project.getPluginManager().apply(ByteBuddyAndroidPlugin.class);
     }
 
     private void addSdkDependency(Project project) {

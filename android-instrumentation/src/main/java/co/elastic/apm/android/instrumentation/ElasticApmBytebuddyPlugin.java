dc59bc2124ee6ba0ab2b6ada9ea12d552ba5e278
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-04 15:35:08 +0200

    Adding advice to OkHttpClient

--- android-instrumentation/src/main/java/co/elastic/apm/android/instrumentation/ElasticApmBytebuddyPlugin.java
+++ android-instrumentation/src/main/java/co/elastic/apm/android/instrumentation/ElasticApmBytebuddyPlugin.java
@@ -1,11 +1,15 @@
 package co.elastic.apm.android.instrumentation;
 
+import net.bytebuddy.asm.Advice;
 import net.bytebuddy.build.Plugin;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.matcher.ElementMatchers;
 
 import java.io.IOException;
+
+import okhttp3.OkHttpClient;
 
 public class ElasticApmBytebuddyPlugin implements Plugin {
 
@@ -13,8 +17,10 @@ public class ElasticApmBytebuddyPlugin implements Plugin {
     public DynamicType.Builder<?> apply(DynamicType.Builder<?> builder,
                                         TypeDescription typeDescription,
                                         ClassFileLocator classFileLocator) {
-        System.out.println("Type that matched: " + typeDescription.toString());//todo delete
-        return builder;
+        return builder.visit(Advice.to(OkHttpClientAdvice.class)
+                .on(ElementMatchers.isConstructor()
+                        .and(ElementMatchers.takesArguments(OkHttpClient.Builder.class))
+                ));
     }
 
     @Override
@@ -24,6 +30,6 @@ public class ElasticApmBytebuddyPlugin implements Plugin {
 
     @Override
     public boolean matches(TypeDescription target) {
-        return target.getTypeName().equals("okhttp3.OkHttpClient$Builder");
+        return target.getTypeName().equals("okhttp3.OkHttpClient");
     }
 }
---
+++ android-instrumentation/src/main/java/co/elastic/apm/android/instrumentation/OkHttpClientAdvice.java
@@ -0,0 +1,13 @@
+package co.elastic.apm.android.instrumentation;
+
+import net.bytebuddy.asm.Advice;
+
+import okhttp3.OkHttpClient;
+
+public class OkHttpClientAdvice {
+
+    @Advice.OnMethodEnter
+    public static void enter(@Advice.Argument(0) OkHttpClient.Builder builder) {
+
+    }
+}

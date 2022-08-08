8cd83b484ac57ccc6c599d844f8a824953687a43
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-08 14:26:00 +0200

    Renaming okhttp event listener

--- android-instrumentation/src/main/java/co/elastic/apm/android/instrumentation/OkHttpClientAdvice.java
+++ android-instrumentation/src/main/java/co/elastic/apm/android/instrumentation/OkHttpClientAdvice.java
@@ -4,14 +4,14 @@ import android.util.Log;
 
 import net.bytebuddy.asm.Advice;
 
-import co.elastic.apm.android.sdk.okhttp.OkhttpEventListener;
+import co.elastic.apm.android.sdk.okhttp.OtelEventListener;
 import okhttp3.OkHttpClient;
 
 public class OkHttpClientAdvice {
 
     @Advice.OnMethodEnter
     public static void enter(@Advice.Argument(0) OkHttpClient.Builder builder) {
-        builder.eventListener(new OkhttpEventListener());
+        builder.eventListener(new OtelEventListener());
         Log.d("cesar", "Event listener is set");
     }
 }

24daec0423ad6a31b9d2f5c9963fe404bd01f444
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-08 13:46:13 +0200

    Setting up spans in OkhttpEventListener

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/okhttp/OkhttpEventListener.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/okhttp/OkhttpEventListener.java
@@ -1,29 +1,81 @@
 package co.elastic.apm.android.sdk.okhttp;
 
-import android.util.Log;
-
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+import co.elastic.apm.android.sdk.ElasticApmAgent;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 import okhttp3.Call;
 import okhttp3.EventListener;
+import okhttp3.HttpUrl;
+import okhttp3.Request;
 
 public class OkhttpEventListener extends EventListener {
+
+    private static final String SPAN_NAME_FORMAT = "%s %s";
+    private final Map<Request, Context> spanContexts = Collections.synchronizedMap(new HashMap<>());
 
     @Override
     public void callStart(Call call) {
         super.callStart(call);
-        Log.d("cesar", "CALL STARTED");
+        Context currentContext = Context.current();
+        Request request = call.request();
+        String method = request.method();
+        HttpUrl url = request.url();
+        String host = url.host();
+        Span span = ElasticApmAgent.getTracer().spanBuilder(String.format(SPAN_NAME_FORMAT, method, host))
+                .setSpanKind(SpanKind.CLIENT)
+                .setParent(currentContext)
+                .startSpan();
+        span.setAttribute(SemanticAttributes.HTTP_URL, url.toString());
+        span.setAttribute(SemanticAttributes.HTTP_METHOD, method);
+        span.setAttribute(SemanticAttributes.HTTP_SCHEME, url.scheme());
+        span.setAttribute(SemanticAttributes.HTTP_HOST, host);
+        Context spanContext = currentContext.with(span);
+        spanContexts.put(request, spanContext);
     }
 
     @Override
     public void callEnd(Call call) {
         super.callEnd(call);
-        Log.d("cesar", "CALL ENDED");
+        Request request = call.request();
+        Context context = getContext(request);
+        if (context != null) {
+            Span span = Span.fromContext(context);
+            if (isValid(span)) {
+                span.end();
+            }
+            spanContexts.remove(request);
+        }
     }
 
     @Override
     public void callFailed(Call call, IOException ioe) {
         super.callFailed(call, ioe);
-        Log.d("cesar", "Call failed");
+        Request request = call.request();
+        Context context = getContext(request);
+        if (context != null) {
+            Span span = Span.fromContext(context);
+            if (isValid(span)) {
+                span.setStatus(StatusCode.ERROR);
+                span.recordException(ioe);
+                span.end();
+            }
+            spanContexts.remove(request);
+        }
+    }
+
+    private boolean isValid(Span span) {
+        return span != null && span != Span.getInvalid();
+    }
+
+    private Context getContext(Request request) {
+        return spanContexts.get(request);
     }
 }

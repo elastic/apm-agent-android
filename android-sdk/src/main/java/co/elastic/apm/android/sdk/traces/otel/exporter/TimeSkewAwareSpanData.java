573ef658b6e533d0c8a92a1139ede21677cb8864
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-11 16:03:33 +0200

    Overriding TimeSkewAwareSpanData.getResource instead of attributes

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/otel/exporter/TimeSkewAwareSpanData.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/otel/exporter/TimeSkewAwareSpanData.java
@@ -2,7 +2,9 @@ package co.elastic.apm.android.sdk.traces.otel.exporter;
 
 import java.util.concurrent.TimeUnit;
 
+import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.data.DelegatingSpanData;
 import io.opentelemetry.sdk.trace.data.SpanData;
 
@@ -13,9 +15,8 @@ class TimeSkewAwareSpanData extends DelegatingSpanData {
     }
 
     @Override
-    public Attributes getAttributes() {
-        return Attributes.builder().putAll(super.getAttributes())
-                .put("telemetry.sdk.elastic_export_timestamp", TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis()))
-                .build();
+    public Resource getResource() {
+        return super.getResource()
+                .merge(Resource.create(Attributes.of(AttributeKey.longKey("telemetry.sdk.elastic_export_timestamp"), TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis()))));
     }
 }

a22b3ddbd89a9ee63434bc112445d7ef60aa70ab
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-25 11:56:40 +0200

    Setting up NetworkType in manager

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/data/network/NetworkManager.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/data/network/NetworkManager.java
@@ -2,16 +2,24 @@ package co.elastic.apm.android.sdk.data.network;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.telephony.TelephonyManager;
+
+import androidx.annotation.NonNull;
 
 import co.elastic.apm.android.sdk.data.network.type.NetworkType;
+import co.elastic.apm.android.sdk.data.network.utils.CellSubTypeProvider;
 
 public class NetworkManager extends ConnectivityManager.NetworkCallback {
     private final ConnectivityManager connectivityManager;
-    private NetworkType networkType = NetworkType.None.INSTANCE;
+    private final TelephonyManager telephonyManager;
+    private NetworkType networkType = NetworkType.none();
 
     public NetworkManager(Context context) {
         Context appContext = context.getApplicationContext();
         connectivityManager = (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        telephonyManager = (TelephonyManager) appContext.getSystemService(Context.TELEPHONY_SERVICE);
     }
 
     public void init() {
@@ -24,5 +32,27 @@ public class NetworkManager extends ConnectivityManager.NetworkCallback {
 
     public NetworkType getType() {
         return networkType;
+    }
+
+    @Override
+    public void onCapabilitiesChanged(@NonNull Network network, @NonNull NetworkCapabilities networkCapabilities) {
+        super.onCapabilitiesChanged(network, networkCapabilities);
+        networkType = getNetworkType(networkCapabilities);
+    }
+
+    private NetworkType getNetworkType(NetworkCapabilities networkCapabilities) {
+        if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+            return NetworkType.cell(CellSubTypeProvider.getSubtypeName(telephonyManager));
+        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+            return NetworkType.wifi();
+        } else {
+            return NetworkType.unknown();
+        }
+    }
+
+    @Override
+    public void onLost(@NonNull Network network) {
+        super.onLost(network);
+        networkType = NetworkType.none();
     }
 }

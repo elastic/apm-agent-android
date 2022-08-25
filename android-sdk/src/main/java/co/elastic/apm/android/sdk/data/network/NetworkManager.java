124a47c99c2978d0a33f8e42ae0a261d59005159
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-25 10:18:37 +0200

    Initializing NetworkManager

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/data/network/NetworkManager.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/data/network/NetworkManager.java
@@ -1,4 +1,28 @@
 package co.elastic.apm.android.sdk.data.network;
 
-public class NetworkManager {
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import co.elastic.apm.android.sdk.data.network.type.NetworkType;
+
+public class NetworkManager extends ConnectivityManager.NetworkCallback {
+    private final ConnectivityManager connectivityManager;
+    private NetworkType networkType = NetworkType.None.INSTANCE;
+
+    public NetworkManager(Context context) {
+        Context appContext = context.getApplicationContext();
+        connectivityManager = (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    public void init() {
+        connectivityManager.registerDefaultNetworkCallback(this);
+    }
+
+    public void cleanUp() {
+        connectivityManager.unregisterNetworkCallback(this);
+    }
+
+    public NetworkType getType() {
+        return networkType;
+    }
 }
--- android-sdk/src/main/java/co/elastic/apm/android/sdk/data/network/type/NetworkType.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/data/network/type/NetworkType.java
@@ -2,6 +2,24 @@ package co.elastic.apm.android.sdk.data.network.type;
 
 public interface NetworkType {
 
+    final class None implements NetworkType {
+
+        public static None INSTANCE = new None();
+
+        private None() {
+        }
+
+        @Override
+        public String getName() {
+            return "unavailable";
+        }
+
+        @Override
+        public String getSubTypeName() {
+            return null;
+        }
+    }
+
     String getName();
 
     String getSubTypeName();

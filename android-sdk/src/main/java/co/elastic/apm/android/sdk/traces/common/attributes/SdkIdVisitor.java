0920768dc2fae71f35a2836fa377b57c242e515c
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-26 10:19:30 +0200

    Using semconv common attributes names

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/common/attributes/SdkIdVisitor.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/common/attributes/SdkIdVisitor.java
@@ -3,13 +3,14 @@ package co.elastic.apm.android.sdk.traces.common.attributes;
 import co.elastic.apm.android.sdk.BuildConfig;
 import co.elastic.apm.android.sdk.attributes.AttributesBuilderVisitor;
 import io.opentelemetry.api.common.AttributesBuilder;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 
 public class SdkIdVisitor implements AttributesBuilderVisitor {
 
     @Override
     public void visit(AttributesBuilder builder) {
-        builder.put("telemetry.sdk.name", "android")
-                .put("telemetry.sdk.version", BuildConfig.APM_AGENT_VERSION)
-                .put("telemetry.sdk.language", "java");
+        builder.put(ResourceAttributes.TELEMETRY_SDK_NAME, "android")
+                .put(ResourceAttributes.TELEMETRY_SDK_VERSION, BuildConfig.APM_AGENT_VERSION)
+                .put(ResourceAttributes.TELEMETRY_SDK_LANGUAGE, "java");
     }
 }

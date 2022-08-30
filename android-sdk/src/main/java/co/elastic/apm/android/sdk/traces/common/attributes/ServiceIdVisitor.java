b1b631e8e859f213b266bf303fdacdbee1cf3f36
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-30 15:38:16 +0200

    Getting apm metadata using service inside ServiceIdVisitor

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/common/attributes/ServiceIdVisitor.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/common/attributes/ServiceIdVisitor.java
@@ -2,12 +2,10 @@ package co.elastic.apm.android.sdk.traces.common.attributes;
 
 import android.content.Context;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import co.elastic.apm.android.common.ApmInfo;
+import co.elastic.apm.android.sdk.ElasticApmAgent;
 import co.elastic.apm.android.sdk.attributes.AttributesBuilderVisitor;
+import co.elastic.apm.android.sdk.services.Service;
+import co.elastic.apm.android.sdk.services.metadata.ApmMetadataService;
 import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 
@@ -24,22 +22,15 @@ public class ServiceIdVisitor implements AttributesBuilderVisitor {
 
     @Override
     public void visit(AttributesBuilder builder) {
-        Properties apmInfoProperties = getApmInfoProperties(appContext);
+        ApmMetadataService metadata = getApmMetadataService();
         String serviceName = (this.serviceName != null) ? this.serviceName : appContext.getPackageName();
-        String serviceVersion = (this.serviceVersion != null) ? this.serviceVersion : apmInfoProperties.getProperty(ApmInfo.KEY_VERSION);
+        String serviceVersion = (this.serviceVersion != null) ? this.serviceVersion : metadata.getServiceVersion();
         builder.put(ResourceAttributes.SERVICE_NAME, serviceName)
                 .put(ResourceAttributes.SERVICE_VERSION, serviceVersion)
-                .put(ResourceAttributes.SERVICE_NAMESPACE, apmInfoProperties.getProperty(ApmInfo.KEY_SCOPE_OKHTTP_VERSION))
-                .put(ResourceAttributes.DEPLOYMENT_ENVIRONMENT, apmInfoProperties.getProperty(ApmInfo.KEY_VARIANT_NAME));
+                .put(ResourceAttributes.DEPLOYMENT_ENVIRONMENT, metadata.getDeploymentEnvironment());
     }
 
-    private Properties getApmInfoProperties(Context appContext) {
-        try (InputStream propertiesFileInputStream = appContext.getAssets().open(ApmInfo.ASSET_FILE_NAME)) {
-            Properties properties = new Properties();
-            properties.load(propertiesFileInputStream);
-            return properties;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private ApmMetadataService getApmMetadataService() {
+        return ElasticApmAgent.get().getService(Service.Names.METADATA);
     }
 }

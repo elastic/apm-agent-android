fe35b417557790cbc61f586c6df93993acc2f069
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-09 12:55:27 +0200

    Adding method to iterate through listeners and call methods on them

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/okhttp/compose/CompositeEventListener.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/okhttp/compose/CompositeEventListener.java
@@ -1,5 +1,9 @@
 package co.elastic.apm.android.sdk.traces.okhttp.compose;
 
+import androidx.annotation.NonNull;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import okhttp3.EventListener;
@@ -9,5 +13,15 @@ public class CompositeEventListener extends EventListener {
 
     public CompositeEventListener(List<EventListener> listeners) {
         this.listeners = listeners;
+    }
+
+    public void doCall(@NonNull Method method, @NonNull Object[] params) {
+        for (EventListener listener : listeners) {
+            try {
+                method.invoke(listener, params);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }

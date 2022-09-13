b38c791c82c49a0669310c25b9031ccdce79be3d
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-13 09:00:23 +0200

    Created local compilation helper plugin

---
+++ buildSrc/src/main/java/co/elastic/apm/compile/tools/ApmProjectPlugin.java
@@ -0,0 +1,11 @@
+package co.elastic.apm.compile.tools;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class ApmProjectPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+    }
+}

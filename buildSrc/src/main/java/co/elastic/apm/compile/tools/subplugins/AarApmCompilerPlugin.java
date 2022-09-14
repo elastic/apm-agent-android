7a9b0f3b5b401f99ccec4ec5e5c8ea1abb6efd8a
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-14 18:51:14 +0200

    Adding task to merge notice files for aar projects

--- buildSrc/src/main/java/co/elastic/apm/compile/tools/subplugins/AarApmCompilerPlugin.java
+++ buildSrc/src/main/java/co/elastic/apm/compile/tools/subplugins/AarApmCompilerPlugin.java
@@ -5,8 +5,10 @@ import com.android.build.api.variant.AndroidComponentsExtension;
 import com.android.build.api.variant.Variant;
 
 import org.gradle.api.Project;
+import org.gradle.api.tasks.TaskProvider;
 
 import co.elastic.apm.compile.tools.tasks.NoticeFilesCollectorTask;
+import co.elastic.apm.compile.tools.tasks.NoticeMergerTask;
 import co.elastic.apm.compile.tools.tasks.PomLicensesCollectorTask;
 
 public class AarApmCompilerPlugin extends BasePlugin {
@@ -23,9 +25,13 @@ public class AarApmCompilerPlugin extends BasePlugin {
                 task.getLicensesFound().set(project.getLayout().getBuildDirectory().file(task.getName() + "/licenses.txt"));
                 task.getManualLicenseMapping().set(licensesConfig.manualMappingFile);
             });
-            project.getTasks().register(variant.getName() + "NoticeFilesCollector", NoticeFilesCollectorTask.class, task -> {
+            TaskProvider<NoticeFilesCollectorTask> noticeCollector = project.getTasks().register(variant.getName() + "NoticeFilesCollector", NoticeFilesCollectorTask.class, task -> {
                 task.getRuntimeDependencies().set(component.getVariantDependencies().getRuntimeClasspath());
                 task.getOutputDir().set(project.getLayout().getBuildDirectory().dir(task.getName()));
+            });
+            project.getTasks().register(variant.getName() + "NoticeFilesMerger", NoticeMergerTask.class, task -> {
+                task.getNoticeFilesDir().set(noticeCollector.flatMap(NoticeFilesCollectorTask::getOutputDir));
+                task.getOutputFile().set(project.getLayout().getBuildDirectory().file(task.getName() + "/" + "mergedNotice.txt"));
             });
         });
     }

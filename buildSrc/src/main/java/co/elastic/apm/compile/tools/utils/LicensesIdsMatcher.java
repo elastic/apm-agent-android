614effa45269e8f63bd86e54adfbae458ab258d6
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-15 09:52:12 +0200

    Extracting licenses finding functionality

--- buildSrc/src/main/java/co/elastic/apm/compile/tools/utils/LicensesIdsMatcher.java
+++ buildSrc/src/main/java/co/elastic/apm/compile/tools/utils/LicensesIdsMatcher.java
@@ -2,14 +2,9 @@ package co.elastic.apm.compile.tools.utils;
 
 import org.apache.commons.text.similarity.LevenshteinDistance;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 
 public class LicensesIdsMatcher {
     private static Map<String, String> ids;
@@ -50,22 +45,10 @@ public class LicensesIdsMatcher {
 
     private static Map<String, String> findLicensesIds() {
         Map<String, String> ids = new HashMap<>();
-        InputStream resourceStream = LicensesIdsMatcher.class.getResourceAsStream("/licenses_ids.txt");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(resourceStream)));
 
-        try {
-            while (reader.ready()) {
-                String[] parts = reader.readLine().split("\\|");
-                String id = parts[0];
-                String name = parts[1];
-                if (ids.containsKey(id)) {
-                    throw new RuntimeException("Duplicated licence id: " + id);
-                }
-                ids.put(id, curateLicenseName(name));
-            }
-            reader.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        Map<String, String> licenses = LicensesProvider.findLicenses();
+        for (String id : licenses.keySet()) {
+            ids.put(id, curateLicenseName(licenses.get(id)));
         }
 
         return ids;

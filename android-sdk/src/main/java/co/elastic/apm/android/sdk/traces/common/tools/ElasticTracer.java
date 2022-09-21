eea79d7963d661006eed72b3881d1e75cac71416
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-21 16:20:25 +0200

    Created ElasticTracer as tool to provide common tracers

---
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/common/tools/ElasticTracer.java
@@ -0,0 +1,57 @@
+package co.elastic.apm.android.sdk.traces.common.tools;
+
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import co.elastic.apm.android.sdk.ElasticApmAgent;
+import co.elastic.apm.android.sdk.internal.services.Service;
+import co.elastic.apm.android.sdk.internal.services.metadata.ApmMetadataService;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.api.trace.Tracer;
+
+public class ElasticTracer implements Tracer {
+    private final Tracer tracer;
+
+    public ElasticTracer(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    public static ElasticTracer create(@NonNull String name, @Nullable String version) {
+        if (version == null) {
+            return new ElasticTracer(GlobalOpenTelemetry.getTracer(name));
+        } else {
+            return new ElasticTracer(GlobalOpenTelemetry.getTracer(name, version));
+        }
+    }
+
+    public static ElasticTracer create(String name) {
+        return create(name, null);
+    }
+
+    public static ElasticTracer okhttp() {
+        ApmMetadataService service = ElasticApmAgent.get().getService(Service.Names.METADATA);
+        String version = service.getOkHttpVersion();
+        return create("OkHttp", version);
+    }
+
+    public static ElasticTracer androidActivity() {
+        return create("Android Activity", String.valueOf(Build.VERSION.SDK_INT));
+    }
+
+    @Override
+    public SpanBuilder spanBuilder(@NonNull String spanName) {
+        return tracer.spanBuilder(spanName);
+    }
+
+    public SpanBuilder spanBuilder() {
+        return tracer.spanBuilder(getCallerMethodId());
+    }
+
+    private String getCallerMethodId() {
+        StackTraceElement element = Thread.currentThread().getStackTrace()[4];
+        return element.getClassName() + "->" + element.getMethodName();
+    }
+}

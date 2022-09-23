63947705b83393243f1a1f5b320a8afdbe083c58
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-23 10:16:47 +0200

    Instrumenting local project classes only in ActivityLifecyclePlugin

--- android-instrumentation/src/main/java/co/elastic/apm/android/instrumentation/ui/lifecycle/ActivityLifecyclePlugin.java
+++ android-instrumentation/src/main/java/co/elastic/apm/android/instrumentation/ui/lifecycle/ActivityLifecyclePlugin.java
@@ -1,8 +1,10 @@
 package co.elastic.apm.android.instrumentation.ui.lifecycle;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.build.AndroidDescriptor;
 import net.bytebuddy.build.Plugin;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.ClassFileLocator;
@@ -12,6 +14,11 @@ import net.bytebuddy.matcher.ElementMatchers;
 import java.io.IOException;
 
 public class ActivityLifecyclePlugin implements Plugin {
+    private final AndroidDescriptor androidDescriptor;
+
+    public ActivityLifecyclePlugin(AndroidDescriptor androidDescriptor) {
+        this.androidDescriptor = androidDescriptor;
+    }
 
     @Override
     public DynamicType.Builder<?> apply(DynamicType.Builder<?> builder,
@@ -27,10 +34,9 @@ public class ActivityLifecyclePlugin implements Plugin {
 
     @Override
     public boolean matches(TypeDescription target) {
-//        if (target.getPackage() != null && !target.getPackage().getActualName().startsWith("co.elastic.apm.opbeans")) {//todo get package name as arg
-//            return false;
-//        }
-//        return !target.getSimpleName().startsWith("Hilt_") && target.isAssignableTo(Activity.class);
-        return target.getTypeName().equals("co.elastic.apm.opbeans.HomeActivity");
+        if (androidDescriptor.getTypeScope(target) == AndroidDescriptor.TypeScope.EXTERNAL) {
+            return false;
+        }
+        return !target.getSimpleName().startsWith("Hilt_") && target.isAssignableTo(Activity.class);
     }
 }

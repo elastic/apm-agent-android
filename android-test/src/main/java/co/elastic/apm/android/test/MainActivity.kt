376735e2c170f299dae81f8c67e9840faddf487d
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-23 15:26:31 +0200

    Adding android test app

---
+++ android-test/src/main/java/co/elastic/apm/android/test/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.elastic.apm.android.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

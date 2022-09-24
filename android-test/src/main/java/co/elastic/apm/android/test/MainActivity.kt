2919c4772af0d7a451da5b8ce5f603fd250272d9
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-24 12:32:27 +0200

    Clean up

--- android-test/src/main/java/co/elastic/apm/android/test/MainActivity.kt
+++ android-test/src/main/java/co/elastic/apm/android/test/MainActivity.kt
@@ -6,6 +6,5 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }

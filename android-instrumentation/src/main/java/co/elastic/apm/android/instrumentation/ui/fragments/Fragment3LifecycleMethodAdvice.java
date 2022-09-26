885c2fe1f61497b540454aa68b34b06b3a03c01b
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-26 18:19:23 +0200

    Clean up

--- android-instrumentation/src/main/java/co/elastic/apm/android/instrumentation/ui/fragments/Fragment3LifecycleMethodAdvice.java
+++ android-instrumentation/src/main/java/co/elastic/apm/android/instrumentation/ui/fragments/Fragment3LifecycleMethodAdvice.java
@@ -2,8 +2,6 @@ package co.elastic.apm.android.instrumentation.ui.fragments;
 
 import net.bytebuddy.asm.Advice;
 
-import java.lang.reflect.Method;
-
 import co.elastic.apm.android.sdk.internal.instrumentation.LifecycleMultiMethodSpan;
 import co.elastic.apm.android.sdk.traces.common.tools.ElasticTracer;
 
@@ -11,7 +9,6 @@ public class Fragment3LifecycleMethodAdvice {
 
     @Advice.OnMethodEnter
     public static void onMethodEnter(
-            @Advice.Origin Method method,
             @Advice.This Object owner,
             @Advice.Local("elasticSpanWithScope") LifecycleMultiMethodSpan.SpanWithScope spanWithScope) {
         spanWithScope = LifecycleMultiMethodSpan.onMethodEnter(owner, ElasticTracer.androidFragment());
@@ -19,7 +16,6 @@ public class Fragment3LifecycleMethodAdvice {
 
     @Advice.OnMethodExit(onThrowable = Throwable.class)
     public static void onMethodExit(
-            @Advice.Origin Method method,
             @Advice.Local("elasticSpanWithScope") LifecycleMultiMethodSpan.SpanWithScope spanWithScope,
             @Advice.Thrown Throwable thrown) {
         LifecycleMultiMethodSpan.onMethodExit(spanWithScope, thrown, 3);

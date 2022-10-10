f0ab5a0a4cbde10e4dbfed0323d7b7c0a2e4c26e
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-10 18:22:54 +0200

    Connecting spotless action to jar compilation process

--- buildSrc/src/main/java/co/elastic/apm/compile/tools/sourceheader/subplugins/JavaSourceHeaderPlugin.java
+++ buildSrc/src/main/java/co/elastic/apm/compile/tools/sourceheader/subplugins/JavaSourceHeaderPlugin.java
@@ -7,5 +7,6 @@ public class JavaSourceHeaderPlugin extends BaseSourceHeaderPlugin {
     @Override
     public void apply(Project project) {
         super.apply(project);
+        project.getTasks().getByName("classes", task -> task.dependsOn(project.getTasks().named("spotlessApply")));
     }
 }

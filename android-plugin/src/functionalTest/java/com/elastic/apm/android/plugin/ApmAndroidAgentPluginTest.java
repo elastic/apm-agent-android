f06c922ae178cad63fdc904ac113cedbc38837a1
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-17 10:53:34 +0200

    Adding junit to android-plugin

--- android-plugin/src/functionalTest/java/com/elastic/apm/android/plugin/ApmAndroidAgentPluginTest.java
+++ android-plugin/src/functionalTest/java/com/elastic/apm/android/plugin/ApmAndroidAgentPluginTest.java
@@ -1,4 +1,16 @@
 package com.elastic.apm.android.plugin;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
 public class ApmAndroidAgentPluginTest {
+
+    @Rule
+    public TemporaryFolder projectTemporaryFolder;
+
+    @Before
+    public void setUp() {
+        projectTemporaryFolder = new TemporaryFolder();
+    }
 }

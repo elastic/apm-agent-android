f25c8dfb6bb7105e0f1b9e770029712dff577e26
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-18 09:11:00 +0200

    Renaming compilation config related test

--- android-common/src/main/java/co/elastic/apm/android/common/ApmInfo.java
+++ android-common/src/main/java/co/elastic/apm/android/common/ApmInfo.java
@@ -20,7 +20,7 @@ package co.elastic.apm.android.common;
 
 public class ApmInfo {
     public static String KEY_SERVICE_VERSION = "service.version";
-    public static String KEY_SERVICE_VARIANT_NAME = "service.variant_name";
+    public static String KEY_SERVICE_NAME = "service.name";
     public static String KEY_SCOPE_OKHTTP_VERSION = "scope.okhttp.version";
     public static String ASSET_FILE_NAME = "co_elastic_apm_android.properties";
 }

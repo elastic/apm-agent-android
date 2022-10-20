a5351f31f068cc5d55a1ee6638e26854a7d3b01c
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-20 14:08:11 +0200

    Ensuring ElasticSpanProcessor::onStart is always called

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/otel/processor/ElasticSpanProcessor.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/traces/otel/processor/ElasticSpanProcessor.java
@@ -52,7 +52,7 @@ public class ElasticSpanProcessor implements SpanProcessor {
 
     @Override
     public boolean isStartRequired() {
-        return original.isStartRequired();
+        return true;
     }
 
     @Override

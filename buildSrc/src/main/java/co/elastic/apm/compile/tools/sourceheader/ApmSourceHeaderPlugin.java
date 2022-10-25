65ec02e432b4481c3d24f05757fb33b14fac27f6
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-25 17:22:39 +0200

    Using BasePlugin

--- buildSrc/src/main/java/co/elastic/apm/compile/tools/sourceheader/ApmSourceHeaderPlugin.java
+++ buildSrc/src/main/java/co/elastic/apm/compile/tools/sourceheader/ApmSourceHeaderPlugin.java
@@ -1,23 +1,17 @@
 package co.elastic.apm.compile.tools.sourceheader;
 
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
-
+import co.elastic.apm.compile.tools.base.BasePlugin;
 import co.elastic.apm.compile.tools.sourceheader.subplugins.AndroidSourceHeaderPlugin;
 import co.elastic.apm.compile.tools.sourceheader.subplugins.JavaSourceHeaderPlugin;
 
-public class ApmSourceHeaderPlugin implements Plugin<Project> {
+public class ApmSourceHeaderPlugin extends BasePlugin {
 
     @Override
-    public void apply(Project project) {
-        if (isAndroidProject(project)) {
+    protected void onApply() {
+        if (isAndroidProject()) {
             project.getPluginManager().apply(AndroidSourceHeaderPlugin.class);
         } else {
             project.getPluginManager().apply(JavaSourceHeaderPlugin.class);
         }
-    }
-
-    private boolean isAndroidProject(Project project) {
-        return project.getExtensions().findByName("android") != null;
     }
 }

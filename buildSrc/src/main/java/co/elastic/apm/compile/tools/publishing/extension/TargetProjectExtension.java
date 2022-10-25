d14ec9056223ec64b836d6588280b22d440dfc03
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-25 18:36:57 +0200

    Setting default values for TargetProjectExtension

--- buildSrc/src/main/java/co/elastic/apm/compile/tools/publishing/extension/TargetProjectExtension.java
+++ buildSrc/src/main/java/co/elastic/apm/compile/tools/publishing/extension/TargetProjectExtension.java
@@ -4,4 +4,8 @@ import org.gradle.api.provider.Property;
 
 public abstract class TargetProjectExtension {
     public abstract Property<Boolean> getDisablePublication();
+
+    public TargetProjectExtension() {
+        getDisablePublication().convention(false);
+    }
 }

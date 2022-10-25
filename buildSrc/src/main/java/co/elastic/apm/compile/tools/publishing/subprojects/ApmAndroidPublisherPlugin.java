f9b322061b45af36a1828ea389c90a59b2df6ad6
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-25 18:29:44 +0200

    Configuring android publication

--- buildSrc/src/main/java/co/elastic/apm/compile/tools/publishing/subprojects/ApmAndroidPublisherPlugin.java
+++ buildSrc/src/main/java/co/elastic/apm/compile/tools/publishing/subprojects/ApmAndroidPublisherPlugin.java
@@ -1,12 +1,17 @@
 package co.elastic.apm.compile.tools.publishing.subprojects;
 
-import org.gradle.api.Plugin;
+import com.android.build.api.dsl.LibraryExtension;
+
 import org.gradle.api.Project;
 
-public class ApmAndroidPublisherPlugin implements Plugin<Project> {
+public class ApmAndroidPublisherPlugin extends BaseApmPublisherPlugin {
 
     @Override
     public void apply(Project project) {
+        LibraryExtension androidExtension = project.getExtensions().getByType(LibraryExtension.class);
+        String componentName = "release";
+        androidExtension.getPublishing().singleVariant(componentName);
 
+        addMavenPublication(project, componentName);
     }
 }

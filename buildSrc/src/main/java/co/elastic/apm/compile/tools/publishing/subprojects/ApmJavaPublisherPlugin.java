c050ff56bac037bcab8c5ab3bf5b216208f5d579
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-26 10:40:33 +0200

    Providing sources and javadoc for java libraries publications

--- buildSrc/src/main/java/co/elastic/apm/compile/tools/publishing/subprojects/ApmJavaPublisherPlugin.java
+++ buildSrc/src/main/java/co/elastic/apm/compile/tools/publishing/subprojects/ApmJavaPublisherPlugin.java
@@ -1,6 +1,7 @@
 package co.elastic.apm.compile.tools.publishing.subprojects;
 
 import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPluginExtension;
 
 public class ApmJavaPublisherPlugin extends BaseApmPublisherPlugin {
 
@@ -8,6 +9,9 @@ public class ApmJavaPublisherPlugin extends BaseApmPublisherPlugin {
     public void apply(Project project) {
         project.afterEvaluate(self -> {
             if (!isAGradlePluginProject(self)) { // The "Gradle Plugin" plugin already creates its maven publication.
+                JavaPluginExtension java = project.getExtensions().getByType(JavaPluginExtension.class);
+                java.withJavadocJar();
+                java.withSourcesJar();
                 addMavenPublication(self, "java");
             }
         });

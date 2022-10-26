d5371aa83695a1ebbcd7bb349278ca553bc14f01
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-26 10:34:07 +0200

    Providing sources and javadoc for android libraries publications

--- buildSrc/src/main/java/co/elastic/apm/compile/tools/publishing/subprojects/ApmAndroidPublisherPlugin.java
+++ buildSrc/src/main/java/co/elastic/apm/compile/tools/publishing/subprojects/ApmAndroidPublisherPlugin.java
@@ -4,13 +4,19 @@ import com.android.build.api.dsl.LibraryExtension;
 
 import org.gradle.api.Project;
 
+import kotlin.Unit;
+
 public class ApmAndroidPublisherPlugin extends BaseApmPublisherPlugin {
 
     @Override
     public void apply(Project project) {
         LibraryExtension androidExtension = project.getExtensions().getByType(LibraryExtension.class);
         String componentName = "release";
-        androidExtension.getPublishing().singleVariant(componentName);
+        androidExtension.getPublishing().singleVariant(componentName, librarySingleVariant -> {
+            librarySingleVariant.withJavadocJar();
+            librarySingleVariant.withSourcesJar();
+            return Unit.INSTANCE;
+        });
 
         project.afterEvaluate(self -> addMavenPublication(self, componentName));
     }

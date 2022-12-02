9403e73920c18a94f6256548c60ac4a59833aca4
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-12-02 14:06:26 +0100

    Enabling ntp cache when initializing it

---
+++ android-sdk/src/test/java/co/elastic/apm/android/sdk/internal/time/NtpManagerTest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.android.sdk.internal.time;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NtpManagerTest {
+
+    @Mock
+    public TrueTimeWrapper trueTimeWrapper;
+    private NtpManager ntpManager;
+
+    @Before
+    public void setUp() {
+        ntpManager = new NtpManager(trueTimeWrapper);
+    }
+
+    @Test
+    public void whenInitializing_setUpSharedPreferenceCache() {
+        ntpManager.initialize();
+
+        verify(trueTimeWrapper).withSharedPreferencesCache();
+    }
+}

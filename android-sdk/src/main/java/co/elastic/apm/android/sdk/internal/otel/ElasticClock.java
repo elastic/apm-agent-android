eb29009de66977e792f38f22dcc71aad8527f38b
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-12-02 17:04:40 +0100

    Initializing ElasticClock

---
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/internal/otel/ElasticClock.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.android.sdk.internal.otel;
+
+import androidx.annotation.VisibleForTesting;
+
+import co.elastic.apm.android.sdk.internal.time.SystemTimeProvider;
+import co.elastic.apm.android.sdk.internal.time.ntp.TrueTimeWrapper;
+import io.opentelemetry.sdk.common.Clock;
+
+public class ElasticClock implements Clock {
+    private final TrueTimeWrapper trueTimeWrapper;
+    private final SystemTimeProvider systemTimeProvider;
+
+    @VisibleForTesting
+    public ElasticClock(TrueTimeWrapper trueTimeWrapper, SystemTimeProvider systemTimeProvider) {
+        this.trueTimeWrapper = trueTimeWrapper;
+        this.systemTimeProvider = systemTimeProvider;
+    }
+
+    public ElasticClock(TrueTimeWrapper trueTimeWrapper) {
+        this(trueTimeWrapper, new SystemTimeProvider());
+    }
+
+    @Override
+    public long now() {
+        return 0;
+    }
+
+    @Override
+    public long nanoTime() {
+        return 0;
+    }
+}

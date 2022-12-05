f90fe2271bcb45630df664b4378541cedab446f5
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-12-05 17:18:52 +0100

    Moved agent injection stubbing to common base test class

--- android-test/android-test-common/src/main/java/co/elastic/apm/android/test/common/BaseTest.java
+++ android-test/android-test-common/src/main/java/co/elastic/apm/android/test/common/BaseTest.java
@@ -1,15 +1,35 @@
 package co.elastic.apm.android.test.common;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
+import org.junit.Before;
+
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import co.elastic.apm.android.sdk.internal.injection.AgentDependenciesInjector;
+import co.elastic.apm.android.sdk.internal.time.ntp.NtpManager;
 import co.elastic.apm.android.test.common.spans.SpanExporterCaptor;
 import io.opentelemetry.sdk.trace.data.SpanData;
 
 public abstract class BaseTest {
+    protected AgentDependenciesInjector injector;
+    protected NtpManager ntpManager;
+
+    @Before
+    public void commonBaseSetUp() throws NoSuchFieldException, IllegalAccessException {
+        injector = mock(AgentDependenciesInjector.class);
+        ntpManager = mock(NtpManager.class);
+        doReturn(ntpManager).when(injector).getNtpManager();
+
+        Field instanceField = AgentDependenciesInjector.class.getDeclaredField("INSTANCE");
+        instanceField.setAccessible(true);
+        instanceField.set(null, injector);
+    }
 
     protected List<SpanData> getRecordedSpans(int amountExpected) {
         List<SpanData> spans = getCapturedSpansOrderedByCreation(getSpanExporter());
--- android-test/app/src/test/java/co/elastic/apm/android/test/testutils/base/BaseRobolectricTest.java
+++ android-test/app/src/test/java/co/elastic/apm/android/test/testutils/base/BaseRobolectricTest.java
@@ -1,18 +1,10 @@
 package co.elastic.apm.android.test.testutils.base;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-
-import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
-import java.lang.reflect.Field;
-
-import co.elastic.apm.android.sdk.internal.injection.AgentDependenciesInjector;
-import co.elastic.apm.android.sdk.internal.time.ntp.NtpManager;
 import co.elastic.apm.android.test.common.BaseTest;
 import co.elastic.apm.android.test.common.spans.SpanExporterCaptor;
 import co.elastic.apm.android.test.testutils.MainApp;
@@ -21,19 +13,6 @@ import co.elastic.apm.android.test.utilities.SpanExporterProvider;
 @Config(application = MainApp.class)
 @RunWith(RobolectricTestRunner.class)
 public abstract class BaseRobolectricTest extends BaseTest {
-    protected AgentDependenciesInjector injector;
-    protected NtpManager ntpManager;
-
-    @Before
-    public void baseSetUp() throws NoSuchFieldException, IllegalAccessException {
-        injector = mock(AgentDependenciesInjector.class);
-        ntpManager = mock(NtpManager.class);
-        doReturn(ntpManager).when(injector).getNtpManager();
-
-        Field instanceField = AgentDependenciesInjector.class.getDeclaredField("INSTANCE");
-        instanceField.setAccessible(true);
-        instanceField.set(null, injector);
-    }
 
     @Override
     protected SpanExporterCaptor getSpanExporter() {

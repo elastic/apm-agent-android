2cfb3e6dc32df9ed1faccf82dc6712e5e8a3032f
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-01-11 10:27:09 +0100

    Created ClasspathProvider implementation for AGP 7.3

---
+++ agp-compatibility/agp-compatibility-7-3/src/main/java/co/elastic/apm/android/agp73/tools/ClasspathProvider73.java
@@ -0,0 +1,38 @@
+package co.elastic.apm.android.agp73.tools;
+
+import com.android.build.api.variant.Variant;
+
+import org.gradle.api.Action;
+import org.gradle.api.artifacts.ArtifactView;
+import org.gradle.api.attributes.Attribute;
+import org.gradle.api.file.FileCollection;
+
+import co.elastic.apm.android.agp.api.tools.ClasspathProvider;
+
+public class ClasspathProvider73 implements ClasspathProvider, Action<ArtifactView.ViewConfiguration> {
+
+    private static final Attribute<String> ARTIFACT_TYPE_ATTR = Attribute.of("artifactType", String.class);
+    private FileCollection runtimeClasspath;
+
+    @Override
+    public FileCollection getRuntimeClasspath(Variant variant) {
+        if (runtimeClasspath == null) {
+            runtimeClasspath = findClasspath(variant);
+        }
+
+        return runtimeClasspath;
+    }
+
+    private FileCollection findClasspath(Variant variant) {
+        return variant.getRuntimeConfiguration().getIncoming()
+                .artifactView(this)
+                .getArtifacts()
+                .getArtifactFiles();
+    }
+
+    @Override
+    public void execute(ArtifactView.ViewConfiguration configuration) {
+        configuration.setLenient(false);
+        configuration.getAttributes().attribute(ARTIFACT_TYPE_ATTR, "android-classes-jar");
+    }
+}

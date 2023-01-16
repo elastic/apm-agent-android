358da759dec0bfd6c31636a77a2a699ddd415ca8
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-01-16 15:20:30 +0100

    Created log verifier

---
+++ android-test/android-test-common/src/main/java/co/elastic/apm/android/test/common/logs/verifiers/LogVerifier.java
@@ -0,0 +1,30 @@
+package co.elastic.apm.android.test.common.logs.verifiers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.sdk.logs.data.LogRecordData;
+
+public class LogVerifier {
+    private final LogRecordData log;
+
+    public LogVerifier(LogRecordData log) {
+        this.log = log;
+    }
+
+    public LogVerifier hasResource(String resourceName) {
+        assertNotNull(log.getResource().getAttribute(AttributeKey.stringKey(resourceName)));
+        return this;
+    }
+
+    public LogVerifier hasResource(String resourceName, String resourceValue) {
+        assertEquals(resourceValue, log.getResource().getAttribute(AttributeKey.stringKey(resourceName)));
+        return this;
+    }
+
+    public LogVerifier startedAt(long timeInNanoseconds) {
+        assertEquals(timeInNanoseconds, log.getEpochNanos());
+        return this;
+    }
+}

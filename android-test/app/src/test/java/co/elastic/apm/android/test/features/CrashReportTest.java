01dd168b2c085059a124feea483c1489022feef5
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-01-17 18:01:43 +0100

    Updated tests to check elastic exception handler's delegation to original handler if any

--- android-test/app/src/test/java/co/elastic/apm/android/test/features/CrashReportTest.java
+++ android-test/app/src/test/java/co/elastic/apm/android/test/features/CrashReportTest.java
@@ -1,12 +1,20 @@
 package co.elastic.apm.android.test.features;
 
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.verify;
+
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import co.elastic.apm.android.sdk.ElasticApmAgent;
 import co.elastic.apm.android.test.common.logs.Logs;
 import co.elastic.apm.android.test.testutils.base.BaseRobolectricTest;
+import co.elastic.apm.android.test.testutils.base.BaseRobolectricTestApplication;
 import io.opentelemetry.sdk.logs.data.LogRecordData;
 
 public class CrashReportTest extends BaseRobolectricTest {
@@ -23,6 +31,31 @@ public class CrashReportTest extends BaseRobolectricTest {
                 .hasExceptionMessage("Custom exception")
                 .hasStacktrace(stackTraceToString(exception))
                 .hasExceptionType("java.lang.IllegalStateException");
+    }
+
+    @Config(application = ApplicationWithExistingExceptionHandler.class)
+    @Test
+    public void whenCrashHappens_and_thereIsAnExistingExceptionHandler_delegateToIt() {
+        IllegalStateException exception = new IllegalStateException("Custom exception");
+        Thread.UncaughtExceptionHandler elasticHandler = Thread.getDefaultUncaughtExceptionHandler();
+        elasticHandler.uncaughtException(Thread.currentThread(), exception);
+
+        ApplicationWithExistingExceptionHandler app = (ApplicationWithExistingExceptionHandler) RuntimeEnvironment.getApplication();
+
+        assertNotEquals(elasticHandler, app.originalExceptionHandler);
+        verify(app.originalExceptionHandler).uncaughtException(Thread.currentThread(), exception);
+    }
+
+    public static class ApplicationWithExistingExceptionHandler extends BaseRobolectricTestApplication {
+        public Thread.UncaughtExceptionHandler originalExceptionHandler;
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            originalExceptionHandler = Mockito.mock(Thread.UncaughtExceptionHandler.class);
+            Thread.setDefaultUncaughtExceptionHandler(originalExceptionHandler);
+            ElasticApmAgent.initialize(this, getConnectivity());
+        }
     }
 
     private String stackTraceToString(Throwable throwable) {

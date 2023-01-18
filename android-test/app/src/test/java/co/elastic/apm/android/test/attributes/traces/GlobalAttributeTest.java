a1a0e51b9331c56b2b87d47d767901ce7a447815
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-01-18 19:02:13 +0100

    Added global span carrier info test

--- android-test/app/src/test/java/co/elastic/apm/android/test/attributes/traces/GlobalAttributeTest.java
+++ android-test/app/src/test/java/co/elastic/apm/android/test/attributes/traces/GlobalAttributeTest.java
@@ -1,10 +1,17 @@
 package co.elastic.apm.android.test.attributes.traces;
 
+import android.content.Context;
+import android.telephony.TelephonyManager;
+
 import org.junit.Test;
+import org.robolectric.Shadows;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowTelephonyManager;
 
 import java.util.List;
 
 import co.elastic.apm.android.test.common.spans.Spans;
+import co.elastic.apm.android.test.testutils.MainApp;
 import co.elastic.apm.android.test.testutils.base.BaseRobolectricTest;
 import io.opentelemetry.sdk.trace.data.SpanData;
 
@@ -26,6 +33,18 @@ public class GlobalAttributeTest extends BaseRobolectricTest {
                 .hasAttribute("type", "mobile");
     }
 
+    @Config(application = AppWithCarrierInfo.class)
+    @Test
+    public void whenASpanIsCreated_verifyItHasCarrierInfoParams() {
+        SpanData customSpan = getSpanData();
+
+        Spans.verify(customSpan)
+                .hasAttribute("net.host.carrier.name", AppWithCarrierInfo.SIM_OPERATOR_NAME)
+                .hasAttribute("net.host.carrier.mcc", "123")
+                .hasAttribute("net.host.carrier.mnc", "456")
+                .hasAttribute("net.host.carrier.icc", AppWithCarrierInfo.SIM_COUNTRY_ISO);
+    }
+
     private SpanData getSpanData() {
         SpanAttrHost host = new SpanAttrHost();
 
@@ -33,5 +52,21 @@ public class GlobalAttributeTest extends BaseRobolectricTest {
 
         List<SpanData> spans = getRecordedSpans(1);
         return spans.get(0);
+    }
+
+    public static class AppWithCarrierInfo extends MainApp {
+        private static final String SIM_OPERATOR = "123456";
+        private static final String SIM_OPERATOR_NAME = "elasticphone";
+        private static final String SIM_COUNTRY_ISO = "us";
+
+        @Override
+        public void onCreate() {
+            ShadowTelephonyManager shadowTelephonyManager = Shadows.shadowOf((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE));
+            shadowTelephonyManager.setSimOperator(SIM_OPERATOR);
+            shadowTelephonyManager.setSimState(TelephonyManager.SIM_STATE_READY);
+            shadowTelephonyManager.setSimOperatorName(SIM_OPERATOR_NAME);
+            shadowTelephonyManager.setSimCountryIso(SIM_COUNTRY_ISO);
+            super.onCreate();
+        }
     }
 }

b2ad6634a41aa277fcfe74f83d13308fe8ffaa51
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-01-31 14:05:04 +0100

    Added test for launch time metric

--- android-test/app/src/test/java/co/elastic/apm/android/test/InitializationTest.java
+++ android-test/app/src/test/java/co/elastic/apm/android/test/InitializationTest.java
@@ -4,14 +4,19 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
+import org.robolectric.Robolectric;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 
 import co.elastic.apm.android.sdk.ElasticApmAgent;
 import co.elastic.apm.android.sdk.ElasticApmConfiguration;
 import co.elastic.apm.android.sdk.session.impl.DefaultSessionIdProvider;
+import co.elastic.apm.android.test.activities.FullCreationActivity;
+import co.elastic.apm.android.test.common.metrics.Metrics;
 import co.elastic.apm.android.test.testutils.base.BaseRobolectricTest;
 import co.elastic.apm.android.test.testutils.base.BaseRobolectricTestApplication;
+import io.opentelemetry.sdk.metrics.data.MetricData;
 
 public class InitializationTest extends BaseRobolectricTest {
 
@@ -21,6 +26,22 @@ public class InitializationTest extends BaseRobolectricTest {
         AppWithMockSessionId app = (AppWithMockSessionId) RuntimeEnvironment.getApplication();
 
         verify(app.sessionIdProvider).initialize();
+    }
+
+    @Test
+    public void whenFirstActivityIsOpen_trackStartupTime_in_onResume() {
+        try (ActivityController<FullCreationActivity> controller = Robolectric.buildActivity(FullCreationActivity.class)) {
+            controller.create().start().postCreate(null);
+
+            getRecordedMetrics(0);
+
+            controller.resume();
+
+            MetricData startupMetric = getRecorderMetric();
+
+            Metrics.verify(startupMetric)
+                    .hasResource("application.launch.time");
+        }
     }
 
     private static class AppWithMockSessionId extends BaseRobolectricTestApplication {

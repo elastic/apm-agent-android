05b6c7d021eb07a4518974ab6ec4dfca70b08312
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-01-31 15:13:26 +0100

    Adding logs

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/internal/features/launchtime/LaunchTimeActivityCallback.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/internal/features/launchtime/LaunchTimeActivityCallback.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import co.elastic.apm.android.common.internal.logging.Elog;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.metrics.DoubleHistogram;
 import io.opentelemetry.api.metrics.Meter;
@@ -24,12 +25,14 @@ public final class LaunchTimeActivityCallback implements Application.ActivityLif
     }
 
     private void sendAppLaunchTimeMetric(long launchTimeInNanos) {
+        Elog.getLogger().info("Sending launch time metric");
         Meter meter = GlobalOpenTelemetry.getMeter("LaunchTimeTracker");
         DoubleHistogram histogram = meter.histogramBuilder("application.launch.time").build();
         histogram.record(launchTimeInNanos);
     }
 
     private void unregisterCallback(@NonNull Activity activity) {
+        Elog.getLogger().debug("Unregistering launch time activity callback");
         activity.getApplication().unregisterActivityLifecycleCallbacks(this);
     }
 

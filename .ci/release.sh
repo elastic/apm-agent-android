0e16272697c6e57d3643b6567dfbb2113c68f7ef
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-02-02 16:23:40 +0100

    release: use new vault secrets

--- .ci/release.sh
+++ .ci/release.sh
@@ -15,11 +15,11 @@ env | sort
 
 echo "--- Prepare vault context"
 set +x
-VAULT_ROLE_ID_SECRET=$(vault read -field=role-id secret/ci/elastic-observability-robots-playground/internal-ci-approle)
+VAULT_ROLE_ID_SECRET=$(vault read -field=role-id secret/ci/elastic-apm-agent-android/internal-ci-approle)
 export VAULT_ROLE_ID_SECRET
-VAULT_SECRET_ID_SECRET=$(vault read -field=secret-id secret/ci/elastic-observability-robots-playground/internal-ci-approle)
+VAULT_SECRET_ID_SECRET=$(vault read -field=secret-id secret/ci/elastic-apm-agent-android/internal-ci-approle)
 export VAULT_SECRET_ID_SECRET
-VAULT_ADDR=$(vault read -field=vault-url secret/ci/elastic-observability-robots-playground/internal-ci-approle)
+VAULT_ADDR=$(vault read -field=vault-url secret/ci/elastic-apm-agent-android/internal-ci-approle)
 export VAULT_ADDR
 
 # Delete the vault specific accessing the ci vault

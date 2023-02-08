3f3ab651140de09fbe87cdc9a45d3cecf58e8aa9
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-02-08 15:04:58 +0100

    Created Instrumentation base class

---
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/instrumentation/Instrumentation.java
@@ -0,0 +1,35 @@
+package co.elastic.apm.android.sdk.instrumentation;
+
+import co.elastic.apm.android.sdk.instrumentation.supported.AppLaunchTimeConfiguration;
+import co.elastic.apm.android.sdk.instrumentation.supported.CrashReportingConfiguration;
+import co.elastic.apm.android.sdk.instrumentation.supported.HttpRequestsConfiguration;
+import co.elastic.apm.android.sdk.instrumentation.supported.ScreenRenderingConfiguration;
+import co.elastic.apm.android.sdk.internal.configuration.FeatureConfiguration;
+
+public abstract class Instrumentation extends FeatureConfiguration {
+
+    protected abstract Type getInstrumentationType();
+
+    @Override
+    protected Class<? extends FeatureConfiguration> getParentConfiguration() {
+        switch (getInstrumentationType()) {
+            case HTTP_REQUESTS:
+                return HttpRequestsConfiguration.class;
+            case SCREEN_RENDERING:
+                return ScreenRenderingConfiguration.class;
+            case CRASH_REPORTING:
+                return CrashReportingConfiguration.class;
+            case APP_LAUNCH_TIME:
+                return AppLaunchTimeConfiguration.class;
+        }
+
+        throw new UnsupportedOperationException();
+    }
+
+    public enum Type {
+        HTTP_REQUESTS,
+        SCREEN_RENDERING,
+        CRASH_REPORTING,
+        APP_LAUNCH_TIME
+    }
+}

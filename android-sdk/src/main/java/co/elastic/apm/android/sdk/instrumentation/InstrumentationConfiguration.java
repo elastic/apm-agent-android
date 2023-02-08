aad20474db0d356cb5f53bc5da8991a743a64fe9
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-02-08 11:58:59 +0100

    Created instrumentation configuration interface

---
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/instrumentation/InstrumentationConfiguration.java
@@ -0,0 +1,14 @@
+package co.elastic.apm.android.sdk.instrumentation;
+
+import co.elastic.apm.android.sdk.internal.configuration.FeatureConfiguration;
+
+public interface InstrumentationConfiguration {
+
+    FeatureConfiguration getHttpRequestsConfiguration();
+
+    FeatureConfiguration getScreenRenderingConfiguration();
+
+    FeatureConfiguration getCrashReportingConfiguration();
+
+    FeatureConfiguration getAppLaunchTimeConfiguration();
+}

7d8a31e12437d72cfaba41d42944baea950547b7
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-02-23 18:57:19 +0100

    Scheduling central config fetching on agent central config initialization error

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/internal/features/centralconfig/initializer/CentralConfigInitializer.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/internal/features/centralconfig/initializer/CentralConfigInitializer.java
@@ -38,6 +38,8 @@ public class CentralConfigInitializer implements BackgroundExecutor.Callback<Int
             } else {
                 CentralConfigurationManager.scheduleInitialSync(context);
             }
+        } else {
+            CentralConfigurationManager.scheduleInitialSync(context);
         }
     }
 }

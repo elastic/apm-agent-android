5f6dee59c417bc3cef38a34069da23ca1c217294
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-04-28 14:43:09 +0200

    Created elastic metric reader

---
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/internal/opentelemetry/processors/metrics/ElasticMetricReader.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package co.elastic.apm.android.sdk.internal.opentelemetry.processors.metrics;
+
+import java.util.Collection;
+
+import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.metrics.InstrumentType;
+import io.opentelemetry.sdk.metrics.data.AggregationTemporality;
+import io.opentelemetry.sdk.metrics.data.MetricData;
+import io.opentelemetry.sdk.metrics.export.CollectionRegistration;
+import io.opentelemetry.sdk.metrics.export.MetricReader;
+import io.opentelemetry.sdk.metrics.internal.export.MetricProducer;
+
+public final class ElasticMetricReader implements MetricReader {
+    private final MetricReader wrapped;
+
+    public ElasticMetricReader(MetricReader wrapped) {
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public void register(CollectionRegistration registration) {
+        if (registration instanceof MetricProducer) {
+            wrapped.register(new ElasticMetricProducer((MetricProducer) registration));
+        } else {
+            wrapped.register(registration);
+        }
+    }
+
+    @Override
+    public CompletableResultCode forceFlush() {
+        return wrapped.forceFlush();
+    }
+
+    @Override
+    public CompletableResultCode shutdown() {
+        return wrapped.shutdown();
+    }
+
+    @Override
+    public AggregationTemporality getAggregationTemporality(InstrumentType instrumentType) {
+        return wrapped.getAggregationTemporality(instrumentType);
+    }
+
+    private static class ElasticMetricProducer implements MetricProducer {
+        private final MetricProducer wrapped;
+
+        private ElasticMetricProducer(MetricProducer wrapped) {
+            this.wrapped = wrapped;
+        }
+
+        @Override
+        public Collection<MetricData> collectAllMetrics() {
+            return wrapped.collectAllMetrics();
+        }
+    }
+}

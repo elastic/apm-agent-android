fcdb21510faeec81e1db885e5021c74bf0634e9e
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-08-10 11:59:43 +0200

    Updating time delay for PeriodicWorkService

--- android-sdk/src/main/java/co/elastic/apm/android/sdk/internal/services/periodicwork/PeriodicWorkService.java
+++ android-sdk/src/main/java/co/elastic/apm/android/sdk/internal/services/periodicwork/PeriodicWorkService.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package co.elastic.apm.android.sdk.internal.services.periodicwork;
 
 import java.util.HashSet;
@@ -15,7 +33,7 @@ public class PeriodicWorkService implements Service, Runnable {
     private final Set<PeriodicTask> tasks = new HashSet<>();
     private final AtomicBoolean isStopped = new AtomicBoolean(false);
     private ScheduledExecutorService executorService;
-    private static final long DELAY_BETWEEN_WORK_RUNS_IN_MILLIS = 1000;
+    private static final long DELAY_BETWEEN_WORK_RUNS_IN_MILLIS = 5 * 1000; // 5 seconds
 
     public void addTask(PeriodicTask task) {
         tasks.add(task);
@@ -23,6 +41,7 @@ public class PeriodicWorkService implements Service, Runnable {
 
     @Override
     public void start() {
+        Elog.getLogger().debug("Starting PeriodicWorkService");
         if (isStopped.get()) {
             throw new IllegalStateException("The periodic work service has been stopped");
         }
@@ -55,7 +74,6 @@ public class PeriodicWorkService implements Service, Runnable {
     }
 
     private void scheduleNextWorkRun() {
-        Elog.getLogger().debug("Scheduling next work for PeriodicWorkService");
         executorService.schedule(this, DELAY_BETWEEN_WORK_RUNS_IN_MILLIS, TimeUnit.MILLISECONDS);
     }
 }

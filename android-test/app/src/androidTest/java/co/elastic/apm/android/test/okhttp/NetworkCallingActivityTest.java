d9054b7eb5cf5cacf077b1bbcc85a0c1b881a099
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-01-29 11:01:58 +0100

    Moving okhttp android test network call to the test

--- android-test/app/src/androidTest/java/co/elastic/apm/android/test/okhttp/NetworkCallingActivityTest.java
+++ android-test/app/src/androidTest/java/co/elastic/apm/android/test/okhttp/NetworkCallingActivityTest.java
@@ -2,55 +2,93 @@ package co.elastic.apm.android.test.okhttp;
 
 import static org.junit.Assert.assertNotNull;
 
+import androidx.annotation.NonNull;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import co.elastic.apm.android.test.base.ActivityEspressoTest;
+import co.elastic.apm.android.sdk.traces.ElasticTracers;
+import co.elastic.apm.android.test.base.BaseEspressoTest;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
 
-public class NetworkCallingActivityTest extends ActivityEspressoTest<NetworkCallingActivity> {
+public class NetworkCallingActivityTest extends BaseEspressoTest {
 
-    private MockWebServer mockWebServer;
-    private HttpUrl mockUrl;
+    private MockWebServer webServer;
+
+    @Before
+    public void setUp() {
+        webServer = new MockWebServer();
+        try {
+            webServer.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            webServer.shutdown();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     public void whenOtherOkhttpInterceptorsAreSet_propagateParentContext() throws InterruptedException {
-        activity.makeNetworkCall(mockUrl);
+        webServer.enqueue(new MockResponse().setResponseCode(200).setBody("{}"));
+        makeNetworkCall(webServer.url("/"));
 
-        RecordedRequest recordedRequest = mockWebServer.takeRequest(1, TimeUnit.SECONDS);
+        RecordedRequest recordedRequest = webServer.takeRequest(1, TimeUnit.SECONDS);
 
         assertNotNull(recordedRequest.getHeader("MY-HEADER"));
         assertNotNull(recordedRequest.getHeader("traceparent"));
     }
 
-    @Override
-    protected void onBefore() {
-        mockWebServer = new MockWebServer();
-        try {
-            mockWebServer.start();
-            mockUrl = mockWebServer.url("/");
-            mockWebServer.enqueue(new MockResponse().setBody("{}").setResponseCode(200));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private void makeNetworkCall(HttpUrl mockServerUrl) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(chain -> {
+                    Request request = chain.request();
 
-    @Override
-    protected void onAfter() {
-        try {
-            mockWebServer.shutdown();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+                    return chain.proceed(request.newBuilder().addHeader("MY-HEADER", "My header value").build());
+                }).build();
 
-    @Override
-    protected Class<NetworkCallingActivity> getActivityClass() {
-        return NetworkCallingActivity.class;
+        Request request = new Request.Builder()
+                .url(mockServerUrl)
+                .build();
+
+        Span span = ElasticTracers.androidActivity().spanBuilder("Http parent span").startSpan();
+        try (Scope ignored = span.makeCurrent()) {
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    latch.countDown();
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    latch.countDown();
+                }
+            });
+        }
+        span.end();
+
+        latch.await(1, TimeUnit.SECONDS);
     }
 }

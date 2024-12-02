91860a25820087c75ee0c3ec01406d98692640d3
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-12-02 09:40:16 +0100

    Adding java lib conventions

---
+++ build-tools/src/main/kotlin/java-library-conventions.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+        plugins {
+            id("java-library")
+            id("org.jetbrains.kotlin.jvm")
+        }
+
+val javaVersionStr = project.property("javaCompatibility") as String
+val javaVersion = JavaVersion.toVersion(javaVersionStr)
+java {
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+}
+
+kotlin.compilerOptions {
+    jvmTarget.set(JvmTarget.fromTarget(javaVersionStr))
+}
+
+tasks.withType(Test::class).configureEach {
+    useJUnitPlatform()
+}
+
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+dependencies {
+    testImplementation(libs.findBundle("mocking").get())
+    testImplementation(libs.findBundle("junit").get())
+    testImplementation(libs.findLibrary("assertj").get())
+    testRuntimeOnly(libs.findLibrary("junit5-vintage").get())
+}

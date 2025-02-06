3ea47a8be7764350bb1c7728500115ba2a3036fe
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-02-06 10:06:27 +0100

    Creating elastic.android-test-library

---
+++ build-tools/src/main/kotlin/elastic.android-test-library.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = (project.property("elastic.android.compileSdk") as String).toInt()
+
+    namespace = "co.elastic.otel.android.test"
+
+    defaultConfig {
+        minSdk = (project.property("elastic.android.minSdk") as String).toInt()
+    }
+
+    val javaVersionStr = project.property("elastic.java.compatibility") as String
+    val javaVersion = JavaVersion.toVersion(javaVersionStr)
+    compileOptions {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = javaVersionStr
+    }
+}
+
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+dependencies {
+    testImplementation(libs.findLibrary("junit4").get())
+}
--- test-tools/agent-rule/build.gradle.kts
+++ test-tools/agent-rule/build.gradle.kts
@@ -1,25 +1,8 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "co.elastic.otel.android.test"
-    compileSdk = (project.property("elastic.android.compileSdk") as String).toInt()
-
-    val javaVersionStr = project.property("elastic.java.compatibility") as String
-    val javaVersion = JavaVersion.toVersion(javaVersionStr)
-    compileOptions {
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-    }
-    kotlinOptions {
-        jvmTarget = javaVersionStr
-    }
+    id("elastic.android-test-library")
 }
 
 dependencies {
     implementation(project(":android-sdk"))
     implementation(libs.opentelemetry.testing)
-    implementation(libs.junit4)
 }

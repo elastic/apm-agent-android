e93e86d84ed1e14c0021490b78f6f944a1a0364c
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-02-10 09:06:21 +0100

    Ignore lint issue

--- test-tools/androidtest-agent-rule/src/main/java/co/elastic/otel/android/test/rule/AndroidTestAgentRule.kt
+++ test-tools/androidtest-agent-rule/src/main/java/co/elastic/otel/android/test/rule/AndroidTestAgentRule.kt
@@ -1,11 +1,13 @@
 package co.elastic.otel.android.test.rule
 
+import android.annotation.SuppressLint
 import android.app.Application
 import androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread
 import androidx.test.platform.app.InstrumentationRegistry
 
 class AndroidTestAgentRule : AgentRule() {
 
+    @SuppressLint("RestrictedApi")
     override fun runInitialization(initialization: () -> Unit) {
         runOnUiThread {
             initialization()

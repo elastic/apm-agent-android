a1f0ca994b31178f7c98aafe88ad15496a082520
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-02-11 15:08:33 +0100

    Renaming http spans

--- android-sdk/src/main/java/co/elastic/otel/android/internal/features/httpinterceptor/HttpSpanNameInterceptor.kt
+++ android-sdk/src/main/java/co/elastic/otel/android/internal/features/httpinterceptor/HttpSpanNameInterceptor.kt
@@ -1,11 +1,36 @@
 package co.elastic.otel.android.internal.features.httpinterceptor
 
 import co.elastic.otel.android.interceptor.Interceptor
+import io.opentelemetry.sdk.trace.data.DelegatingSpanData
 import io.opentelemetry.sdk.trace.data.SpanData
+import io.opentelemetry.semconv.UrlAttributes
 
 class HttpSpanNameInterceptor : Interceptor<SpanData> {
+    private companion object {
+        private val URL_PATTERN = Regex("https?://([^/]+).*")
+    }
 
     override fun intercept(item: SpanData): SpanData {
-        TODO("Not yet implemented")
+        val url = item.attributes.get(UrlAttributes.URL_FULL) ?: return item
+
+        return getNewName(item.name, url)?.let { newName ->
+            NameDelegatingSpanData(item, newName)
+        } ?: item
+    }
+
+    private fun getNewName(name: String, url: String): String? {
+        return URL_PATTERN.matchEntire(url)?.let { match ->
+            "$name ${match.groupValues[1]}"
+        }
+    }
+
+    private class NameDelegatingSpanData(
+        delegate: SpanData,
+        private val name: String
+    ) : DelegatingSpanData(delegate) {
+
+        override fun getName(): String {
+            return name
+        }
     }
 }

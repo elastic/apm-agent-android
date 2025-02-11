6dc48fe255866c9b8a353ed885823146138c28a2
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-02-11 13:34:55 +0100

    Creating http span interceptors

---
+++ android-sdk/src/main/java/co/elastic/otel/android/internal/features/httpinterceptor/HttpInterceptorSpanExporter.kt
@@ -0,0 +1,38 @@
+package co.elastic.otel.android.internal.features.httpinterceptor
+
+import co.elastic.otel.android.interceptor.Interceptor
+import io.opentelemetry.sdk.common.CompletableResultCode
+import io.opentelemetry.sdk.trace.data.SpanData
+import io.opentelemetry.sdk.trace.export.SpanExporter
+import io.opentelemetry.semconv.UrlAttributes
+
+internal class HttpInterceptorSpanExporter(
+    private val delegate: SpanExporter,
+    private val httpSpanInterceptor: Interceptor<SpanData>
+) : SpanExporter {
+
+    override fun export(spans: Collection<SpanData>): CompletableResultCode {
+        val processedSpans = mutableListOf<SpanData>()
+
+        spans.forEach {
+            processedSpans.add(process(it))
+        }
+
+        return delegate.export(processedSpans)
+    }
+
+    private fun process(spanData: SpanData): SpanData {
+        if (spanData.attributes.get(UrlAttributes.URL_FULL) != null) {
+            return httpSpanInterceptor.intercept(spanData)
+        }
+        return spanData
+    }
+
+    override fun flush(): CompletableResultCode {
+        return delegate.flush()
+    }
+
+    override fun shutdown(): CompletableResultCode {
+        return delegate.shutdown()
+    }
+}
---
+++ android-sdk/src/main/java/co/elastic/otel/android/internal/features/httpinterceptor/HttpSpanExporterInterceptor.kt
@@ -0,0 +1,13 @@
+package co.elastic.otel.android.internal.features.httpinterceptor
+
+import co.elastic.otel.android.interceptor.Interceptor
+import io.opentelemetry.sdk.trace.data.SpanData
+import io.opentelemetry.sdk.trace.export.SpanExporter
+
+internal class HttpSpanExporterInterceptor(private val httpSpanInterceptor: Interceptor<SpanData>) :
+    Interceptor<SpanExporter> {
+
+    override fun intercept(item: SpanExporter): SpanExporter {
+        return HttpInterceptorSpanExporter(item, httpSpanInterceptor)
+    }
+}

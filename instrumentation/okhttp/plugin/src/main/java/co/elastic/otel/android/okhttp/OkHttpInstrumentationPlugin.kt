6c004cde7633f6226e1e5bd9adaa53e1c095d0a1
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-02-12 18:03:40 +0100

    Updating instrumentation plugins

--- instrumentation/okhttp/plugin/src/main/java/co/elastic/otel/android/okhttp/OkHttpInstrumentationPlugin.kt
+++ instrumentation/okhttp/plugin/src/main/java/co/elastic/otel/android/okhttp/OkHttpInstrumentationPlugin.kt
@@ -1,12 +1,13 @@
 package co.elastic.otel.android.okhttp
 
 import co.elastic.otel.android.instrumentation.generated.BuildConfig
+import co.elastic.otel.android.plugin.ElasticAgentPlugin
 import co.elastic.otel.android.plugin.internal.InstrumentationPlugin
 import org.gradle.api.Project
 
 class OkHttpInstrumentationPlugin : InstrumentationPlugin() {
 
-    override fun onApply(target: Project) {
+    override fun onApply(target: Project, agentPlugin: ElasticAgentPlugin) {
         target.dependencies.add(
             "implementation",
             target.dependencies.create(BuildConfig.LIBRARY_URI)

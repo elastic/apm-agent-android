abb29af76010cd57fea2acecba7136c14c14791d
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-02-14 09:44:57 +0100

    Clean up

--- sample-app/app/build.gradle.kts
+++ sample-app/app/build.gradle.kts
@@ -36,17 +36,9 @@ android {
 }
 
 dependencies {
-    val lifecycle_version = "2.4.0"
-    val retrofit_version = "2.11.0"
-    implementation("androidx.lifecycle:lifecycle-viewmodel:$lifecycle_version")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
-    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
-    testImplementation("junit:junit:4.13.2")
+    val retrofitVersion = "2.11.0"
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.7")
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 }

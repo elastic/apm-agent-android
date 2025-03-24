0e066c636a87ea55329365243ee3f83bccec9147
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-03-24 09:33:08 +0100

    Adding docs to extensions

--- agent-sdk/src/main/java/co/elastic/otel/android/extensions/ElasticOtelAgentExtensions.kt
+++ agent-sdk/src/main/java/co/elastic/otel/android/extensions/ElasticOtelAgentExtensions.kt
@@ -30,6 +30,18 @@ import java.time.Instant
 
 private const val DEFAULT_SCOPE_NAME = "co.elastic.otel.android.extensions"
 
+/**
+ * Convenience method to send OpenTelemetry's [Log Records](https://opentelemetry.io/docs/concepts/signals/logs/).
+ *
+ * @param body The log body/text.
+ * @param severity The log's severity. Defaults to OpenTelemetry SDK's default.
+ * @param severityText The log's severity text. Defaults to OpenTelemetry SDK's default.
+ * @param attributes The log's attributes. Defaults to OpenTelemetry SDK's default.
+ * @param context The log's context. Defaults to OpenTelemetry SDK's default.
+ * @param observedTimestamp The log's observed timestamp. Defaults to OpenTelemetry SDK's default.
+ * @param timestamp The log's timestamp. Defaults to OpenTelemetry SDK's default.
+ * @param scopeName The log's instrumentation scope name. Defaults to [DEFAULT_SCOPE_NAME].
+ */
 fun ElasticOtelAgent.log(
     body: String,
     severity: Severity? = null,
@@ -51,13 +63,23 @@ fun ElasticOtelAgent.log(
     logger.emit()
 }
 
+/**
+ * Convenience method to send OpenTelemetry's [Spans](https://opentelemetry.io/docs/concepts/signals/traces/#spans).
+ *
+ * @param name The span name.
+ * @param attributes The span's attributes. Defaults to OpenTelemetry SDK's default.
+ * @param kind The span's kind. Defaults to OpenTelemetry SDK's default.
+ * @param parentContext The span's parent context. Defaults to OpenTelemetry SDK's default.
+ * @param makeCurrent Whether the span will be automatically set as the "current one" within the thread it's created in, until its body's finished. Defaults to TRUE.
+ * @param scopeName The span's instrumentation scope name. Defaults to [DEFAULT_SCOPE_NAME].
+ * @param body The span's body. The span will start right before executing its body and will end right after its body's finished or an uncaught exception happens.
+ */
 fun ElasticOtelAgent.span(
     name: String,
     attributes: Attributes? = null,
     kind: SpanKind? = null,
     parentContext: Context? = null,
     makeCurrent: Boolean = true,
-    recordException: Boolean = true,
     scopeName: String = DEFAULT_SCOPE_NAME,
     body: (Span) -> Unit
 ) {
@@ -70,11 +92,9 @@ fun ElasticOtelAgent.span(
     val scope: Scope? = if (makeCurrent) span.makeCurrent() else null
     try {
         body(span)
-    } catch (e: Throwable) {
+    } catch (t: Throwable) {
         span.setStatus(StatusCode.ERROR)
-        if (recordException) {
-            span.recordException(e)
-        }
+        throw t
     } finally {
         scope?.close()
         span.end()

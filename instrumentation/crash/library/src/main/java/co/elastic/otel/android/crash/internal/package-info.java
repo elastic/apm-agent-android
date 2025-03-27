7baa2de8a5507a10e2819dba1610b3ea2ac75324
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-03-27 09:46:28 +0100

    Clean up instrumentations

--- instrumentation/crash/library/src/main/java/co/elastic/otel/android/crash/internal/package-info.java
+++
@@ -1,4 +0,0 @@
-@InternalApi
-package co.elastic.otel.android.crash.internal;
-
-import co.elastic.otel.android.common.internal.annotations.InternalApi;
--- instrumentation/launchtime/library/src/main/java/co/elastic/otel/android/launchtime/internal/package-info.java
+++
@@ -1,4 +0,0 @@
-@InternalApi
-package co.elastic.otel.android.launchtime.internal;
-
-import co.elastic.otel.android.common.internal.annotations.InternalApi;

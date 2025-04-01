3636e829d075fe54861127f74691c9a71d314bdd
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-04-01 15:54:22 +0200

    Clran up

--- convert_docs_to_legacy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-ls docs/reference | egrep -o "[^.]+\.md" | sed -n "s/.md//p" | xargs -I '{}' pandoc -f gfm -t asciidoc docs/reference/{}.md -o legacy_docs/{}.asciidoc
-ls legacy_docs | grep .asciidoc | xargs -I '{}' sed -i '' "s,.md,.asciidoc," legacy_docs/{}

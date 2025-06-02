2bddcec195e352603312fe22a9883960820a1c04
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-06-02 06:24:37 +0200

    Removing notice metadata

--- build-tools/src/main/java/co/elastic/otel/android/compilation/tools/plugins/BaseNoticePlugin.java
+++ build-tools/src/main/java/co/elastic/otel/android/compilation/tools/plugins/BaseNoticePlugin.java
@@ -10,5 +10,4 @@ public abstract class BaseNoticePlugin implements Plugin<Project> {
     protected static final String LIBRARY_ELEMENT_LICENSED_DEPENDENCIES = "licensed-dependencies";
     protected static final String CONFIGURATION_NOTICES_COLLECTOR = "noticeFiles";
     protected static final String LIBRARY_ELEMENT_NOTICE_FILES = "notice-files";
-    protected static final String TASK_VERIFY_NOTICE_FILE_NAME = "verifyNoticeFile";
 }

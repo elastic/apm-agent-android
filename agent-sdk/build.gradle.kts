2f8a8cbbfe03781fae1e7c689f4edb20a1f7a729
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-07-02 15:35:48 +0200

    Setting up base for opamp client

--- agent-sdk/build.gradle.kts
+++ agent-sdk/build.gradle.kts
@@ -1,5 +1,14 @@
+import de.undercouch.gradle.tasks.download.DownloadExtension
+import java.net.HttpURLConnection
+import java.net.URL
+
 plugins {
     id("elastic.android-library")
+
+    // For opamp:
+    id("kotlin-kapt")
+    id("de.undercouch.download") version "5.6.0"
+    id("com.squareup.wire") version "5.3.3"
 }
 
 android {
@@ -40,4 +49,66 @@ dependencies {
     testImplementation(libs.opentelemetry.testing)
     testImplementation(libs.robolectric)
     testImplementation(libs.awaitility)
+
+    // For opamp:
+    implementation("com.github.spotbugs:spotbugs-annotations:4.9.3")
+    kapt("com.google.auto.value:auto-value:1.11.0")
+    compileOnly("com.google.auto.value:auto-value-annotations:1.11.0")
+}
+
+// For opamp:
+val opampProtos = tasks.register<DownloadOpampProtos>("opampProtoDownload", download)
+opampProtos.configure {
+    group = "opamp"
+    outputProtosDir.set(project.layout.buildDirectory.dir("opamp/protos"))
+    downloadedZipFile.set(project.layout.buildDirectory.file("intermediate/$name/release.zip"))
+}
+
+wire {
+    java {}
+    sourcePath {
+        srcDir(opampProtos)
+    }
+}
+
+abstract class DownloadOpampProtos @Inject constructor(
+    private val download: DownloadExtension,
+    private val archiveOps: ArchiveOperations,
+    private val fileOps: FileSystemOperations,
+) : DefaultTask() {
+
+    @get:OutputDirectory
+    abstract val outputProtosDir: DirectoryProperty
+
+    @get:Internal
+    abstract val downloadedZipFile: RegularFileProperty
+
+    @TaskAction
+    fun execute() {
+        // Get the latest release tag by following the redirect from GitHub's latest release URL
+        val latestReleaseUrl = "https://github.com/open-telemetry/opamp-spec/releases/latest"
+        val connection = URL(latestReleaseUrl).openConnection() as HttpURLConnection
+        connection.instanceFollowRedirects = false
+        connection.requestMethod = "HEAD"
+
+        val redirectLocation = connection.getHeaderField("Location")
+        connection.disconnect()
+
+        // Extract tag from URL like: https://github.com/open-telemetry/opamp-spec/releases/tag/v0.12.0
+        val latestTag = redirectLocation.substringAfterLast("/")
+        // Download the source code for the latest release
+        val zipUrl = "https://github.com/open-telemetry/opamp-spec/zipball/$latestTag"
+
+        download.run {
+            src(zipUrl)
+            dest(downloadedZipFile)
+        }
+        val protos = archiveOps.zipTree(downloadedZipFile).matching {
+            setIncludes(listOf("**/*.proto"))
+        }
+        fileOps.sync {
+            from(protos.files)
+            into(outputProtosDir)
+        }
+    }
 }

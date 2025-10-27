dcdbfe6a86b2e25f2c22f33e9f9b95c3acb649d5
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-10-27 16:32:35 +0100

    Adjusting to opamp api changes

--- agent-sdk/src/main/java/co/elastic/otel/android/internal/features/diskbuffering/tools/FromDiskExporter.kt
+++ agent-sdk/src/main/java/co/elastic/otel/android/internal/features/diskbuffering/tools/FromDiskExporter.kt
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package co.elastic.otel.android.internal.features.diskbuffering.tools
 
 import androidx.annotation.GuardedBy
@@ -7,6 +25,10 @@ import java.util.concurrent.TimeUnit
 import kotlin.time.Duration
 import okio.Closeable
 
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
 class FromDiskExporter<T>(
     private val signalStorage: SignalStorage<T>,
     private val networkExport: (MutableCollection<T>) -> CompletableResultCode,

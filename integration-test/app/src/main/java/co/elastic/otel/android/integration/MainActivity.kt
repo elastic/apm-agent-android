c6187d7ec1940f058cef435e602b2faf6d918497
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-11-06 16:21:20 +0100

    Created integration-test subproject

---
+++ integration-test/app/src/main/java/co/elastic/otel/android/integration/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.elastic.otel.android.integration
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

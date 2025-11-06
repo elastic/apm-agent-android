c0e86b7b8c482cf833b610271d3539e550ba7ba3
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-11-06 16:25:53 +0100

    Sending all signals from integration test

--- integration-test/app/src/main/java/co/elastic/otel/android/integration/MainActivity.kt
+++ integration-test/app/src/main/java/co/elastic/otel/android/integration/MainActivity.kt
@@ -2,10 +2,20 @@ package co.elastic.otel.android.integration
 
 import android.app.Activity
 import android.os.Bundle
+import co.elastic.otel.android.extensions.log
+import co.elastic.otel.android.extensions.span
+import co.elastic.otel.android.integration.MyApp.Companion.agent
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val meter = agent.getOpenTelemetry().getMeter("co.elastic.android.integration")
+        meter.counterBuilder("meter counter").build().add(5)
+
+        agent.span("span name") {
+            agent.log("log body")
+        }
     }
 }

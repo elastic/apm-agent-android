3eebdd41ea1affc1133237ae5163696ab22c4f79
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2026-02-27 17:02:24 +0100

    Increase R8 optimization for integration tests

--- integration-test/app/build.gradle.kts
+++ integration-test/app/build.gradle.kts
@@ -18,7 +18,8 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs["debug"]
         }
     }
---
+++ integration-test/app/proguard-rules.pro
@@ -0,0 +1,6 @@
+# Maximize R8 optimizations to surface potential issues with the agent's
+# consumer ProGuard rules. These settings mirror what a heavily optimized
+# production app might use.
+-repackageclasses ''
+-overloadaggressively
+-allowaccessmodification

3b920ec03ad431cc5b74b20f1efd847164b0a738
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2026-02-27 16:56:46 +0100

    Update integration tests

--- integration-test/app/build.gradle.kts
+++ integration-test/app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
             signingConfig = signingConfigs["debug"]
         }
     }
